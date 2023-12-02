dc3f3a55aedcb69c3476c83f7e17569893832315
chanthy <chanthy9@gmail.com>
2023-12-02 08:55:48 +0700

    Final commit

---
+++ app/src/main/java/kh/edu/rupp/ite/khmerculture/MainActivity.java
@@ -0,0 +1,18 @@
+package kh.edu.rupp.ite.khmerculture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+
+
+    }
+}
